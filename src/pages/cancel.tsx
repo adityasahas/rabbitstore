2bd0de965a6221451199c25faee694f449ffe94e
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-06-21 19:20:15 -0700

    fixed build error

--- src/pages/cancel.tsx
+++ src/pages/cancel.tsx
@@ -1,11 +1,11 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Link, Box, Heading, Text } from "@chakra-ui/react";
 
 const CancelPage: React.FC = () => (
   <Box padding="6" boxShadow="lg" bg="white">
     <Heading as="h2" size="xl" mb="2">
       Payment Failed
     </Heading>
-    <Text fontSize="lg">Your payment was not successful. Please try again. <a href="/"><u>Return to home</u></a></Text>
+    <Text fontSize="lg">Your payment was not successful. Please try again. <Link href="/"><u>Return to home</u></Link></Text>
   </Box>
 );
 
