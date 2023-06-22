27e0e29e0fe82adf6b39fb1b0ffd4db4dcc97394
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-06-21 20:56:08 -0700

    changed purchase options

--- src/pages/BuyPage.tsx
+++ src/pages/BuyPage.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/navbar";
 
 const BuyPage: React.FC = () => {
   // Stripe price IDs
+  const priceId0 = "price_1NLebDFfTf4uUx5I3sp3Yehc"; 
   const priceId1 = "price_1NLZstFfTf4uUx5I4jHSlqw3"; 
   const priceId2 = "price_1NLbdmFfTf4uUx5IvFuSIoGs";
   const priceId3 = "price_1NLbe2FfTf4uUx5Io5hiM3pN"; 
@@ -15,18 +16,8 @@ const BuyPage: React.FC = () => {
         <Heading>Buy Our Meat </Heading>
 
         <Box w="200px" borderWidth="1px" borderRadius="lg" p="6" textAlign="center">
-          <Heading size="md">1 Pound ($10)</Heading>
-          <BuyButton priceId={priceId1} />
-        </Box>
-
-        <Box w="200px" borderWidth="1px" borderRadius="lg" p="6" textAlign="center">
-          <Heading size="md">2.5 Pounds ($25)</Heading>
-          <BuyButton priceId={priceId2} />
-        </Box>
-
-        <Box w="200px" borderWidth="1px" borderRadius="lg" p="6" textAlign="center">
-          <Heading size="md">5 Pounds ($50)</Heading>
-          <BuyButton priceId={priceId3} />
+          <Heading size="md">0.5 Pounds ($5)</Heading>
+          <BuyButton priceId={priceId0} />
         </Box>
       </VStack>
     </Flex>
